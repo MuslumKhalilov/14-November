f5704c6a75ec4a8a277f58c4875d20c7808a156d
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-14 02:03:32 +0400

    Task updated

--- 14 November/Program.cs
+++ 14 November/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace _14_November
+﻿using System.Runtime.InteropServices;
+using _14_November.Models;
+using _14_November.Services;
+
+namespace _14_November
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           StudentService studentService = new StudentService();
+
+            Student student = new Student
+            {
+                Name = "Murad",
+                Surname = "Ganbarov",
+                AvgPoint = 90
+            };
+
+            Student student1 = new Student
+            {
+                Name = "Muslum",
+                Surname = "Khalilov",
+                AvgPoint = 90
+            };
+
+
+            //studentService.Create(student);
+
+            //studentService.Create(student1);
+
+            //studentService.Delete(2);
+
+            //Student student2 = studentService.GetById(4);
+            //Console.WriteLine(student2.Name+" "+student2.Surname+" "+student2.AvgPoint);
+
+            //List<Student> students = studentService.GetAll();
+            //foreach (Student student3 in students)
+            //{
+            //    Console.WriteLine(student3.Name + " " + student3.Surname + " " + student3.AvgPoint);
+            //}
+
         }
     }
 }
